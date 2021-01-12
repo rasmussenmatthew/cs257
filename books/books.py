cce349fe1fa95077e178d4847f73192fe92ff33e
Eric Gassel <ericgassel12@gmail.com>
2021-01-12 12:30:12 -0700

    python program created

---
+++ books/books.py
@@ -0,0 +1,12 @@
+def main():
+    print("hello")
+main()
+def authors(name):
+    #authors command
+def titles(name):
+    #titles command
+def years(year1, year2):
+    #names command
+def help1():
+    #print statement TBD
+#NICE
