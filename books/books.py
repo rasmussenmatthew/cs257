605991543f329796acc71315d6f51afddd0ab49f
rasmussenmatthew <rasmussenm@carelton.edu>
2021-01-15 14:35:05 -0600

    Polished functions in books.py

--- books/books.py
+++ books/books.py
@@ -12,37 +12,48 @@ parser.add_argument('-y', '--years', nargs = 2, type = int, metavar='', help = '
 
 args = parser.parse_args()
 
-#implementing new dictionary to be filled below 
-author_dict = {} 
-
-#reading in csv file
-with open('books.csv') as csv_file:
-    csv_reader= csv.reader(csv_file, delimiter=',')
-    #filling the dictionary to connect a list of books to each author
-    for row in csv_reader:
-        if row[2] not in author_dict: #no repeats
-            author_dict[row[2]] = [row[0]]
-        else:
-            author_dict[row[2]].append(row[0])
-
     
 def findAuthors(authors): #authors command
-    for key in author_dict:
+    #implementing new dictionary to be filled below 
+    author_dict = {} 
+
+    #reading in csv file
+    with open('books.csv') as csv_file:
+        csv_reader= csv.reader(csv_file, delimiter=',')
+        #filling the dictionary to connect a list of books to each author
+        for row in csv_reader:
+            if row[2] not in author_dict: #no repeats
+                author_dict[row[2]] = [row[0]]
+            else:
+                author_dict[row[2]].append(row[0])
+                
+    counter = 0        
+    for key in author_dict: #looping through dictionary
         author= str(key) #changing author (including lifespans) to string
         if authors.lower() in author.lower():
+            counter += 1
             print("-",author) #for spacing 
             keyList = author_dict[key]
             for i in range(len(author_dict[key])):
                 print("    ", keyList[i])
             print("             ") #for spacing
+    if counter == 0:
+        print("There is no author whose name conatins the given string")
+        print("             ") #for spacing
     return
 
 def findTitles(titles): #titles command
+    counter = 0 
     with open('books.csv') as csv_file:
         csv_reader= csv.reader(csv_file, delimiter=',')
         for row in csv_reader: 
             if titles.lower() in row[0].lower():
-                print(row[0])    
+                counter += 1
+                print(row[0], "written by", row[2], "in", row[1])    
+                print("             ") #for spacing 
+                
+    if counter == 0: #message to user if no books are found
+        print("There is no book whose title contains the given string.")
     print("             ") #for spacing 
     return 
 
@@ -54,11 +65,17 @@ def findYears(years): #years command: also prints out publish year of book
         yearsB = years[0]
         yearsA = years[1]
         
+    counter = 0 
     with open('books.csv') as csv_file:
         csv_reader= csv.reader(csv_file, delimiter=',')
         for row in csv_reader: 
             if int(row[1]) >= yearsB and int(row[1]) <= yearsA:
-                print(row[0], "-", row[1])
+                counter += 1
+                print(row[0], "written by", row[2], "in", row[1])
+                print("             ") #for spacing 
+                
+    if counter == 0:#message to user if no books are found
+        print("There are no books between the given years")
     print("             ") #for spacing 
     return
 
