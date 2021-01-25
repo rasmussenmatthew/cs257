1b173f43439d9691ecc2d83504b5f7e9c5079bd8
rasmussenmatthew <rasmussenm@carelton.edu>
2021-01-24 22:34:41 -0600

    Fleshed out database-schema.spl

--- database/database-schema.sql
+++ database/database-schema.sql
@@ -1,7 +1,54 @@
-CREATE TABLE table_name(
+CREATE TABLE athletes(
     id SERIAL,
-    something type,
-    something type,
-    something type
+    name text,
+    sex text,
+    age int,
+    height int,
+    weight int
 );
-    
+
+CREATE TABLE nations(
+    id SERIAL,
+    NOC text,
+    team text
+);
+
+CREATE TABLE games(
+    id SERIAL,
+    game_year int,
+    season text,
+    city text
+);
+
+CREATE TABLE medals(
+    id SERIAL,
+    medal text,
+    game_id int,
+    contest_id int
+);
+
+CREATE TABLE contest(
+    id SERIAL,
+    contest text,
+    sport text
+);
+
+CREATE TABLE contests_athletes(
+    contest_id int,
+    athlete_id int
+);
+
+CREATE TABLE medals_athletes(
+    medal_id int,
+    athlete_id int
+);
+
+CREATE TABLE athletes_nations(
+    athelte_id int;
+    nation_id int
+);
+
+CREATE TABLE athletes_games(
+    athletes int,
+    games int
+);
