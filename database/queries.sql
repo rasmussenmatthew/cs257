f62f34fc66215203f715a3e3f3933647395a42e0
rasmussenmatthew <rasmussenm@carelton.edu>
2021-01-25 21:13:15 -0600

    minor adjustments to queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -11,10 +11,14 @@ AND nations.NOC = 'KEN';
 ORDER BY athlete_name
 
 
-SELECT DISTINCT athletes.athlete_name, games.game_year, games.season, games.city, contests_medals.medal                                                     
-FROM athletes, games, contests_medals, athletes_games 
+SELECT DISTINCT athletes.athlete_name, contests_medals.medal, contests.contest, games.game_year, games.season, games.city          
+FROM athletes, games, contests, contests_medals, athletes_games
 WHERE athletes.id = athletes_games.athlete_id
 AND games.id = athletes_games.game_id
+AND athletes_games.id = contests_medals.athletes_nations_games_id
 AND contests_medals.medal IS NOT NULL 
 AND athletes.athlete_name LIKE '%"Greg" Louganis'
 ORDER BY games.game_year;
+
+SELECT nations.noc, COUNT(...)
+
