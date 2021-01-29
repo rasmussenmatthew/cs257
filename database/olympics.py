654ef3d75da5d2d7cef246fb95934954f013f698
rasmussenmatthew <rasmussenm@carelton.edu>
2021-01-29 15:52:13 -0600

    Finalized functions and comments. Added a usage.txt file

--- database/olympics.py
+++ database/olympics.py
@@ -1,6 +1,7 @@
 #Matthew Rasmussen
 #1/29/2021
-#This does things from the command line with my database 
+#Implements three ways of searching through the olympics database
+
 import psycopg2
 from config import password
 from config import database
@@ -10,12 +11,13 @@ import argparse
 
 
 def get_parsed_arguments():
-    '''Takes in user input and returns parsed arguments'''
-    
+    '''
+        Takes in user input and returns parsed arguments
+    '''
     parser = argparse.ArgumentParser(description = 'Search methods for the olympics database')
     parser.add_argument('-a', '--athletes', type = str, metavar=' ', help = 'prints a list of every athlete from a specified NOC (search string)')
     parser.add_argument('-gc', '--gold_count', action = 'store_true', help = 'prints a list of every NOC and a count of gold medals won by their respective representatives')
-    parser.add_argument('-avg', '--average_age', action = 'store_true', help = 'prints the average age of the participants in every sport')
+    parser.add_argument('-avg', '--height_average', action = 'store_true', help = 'prints the average height of the participants in every sport')
     parser.add_argument('-u', '--usage', action = 'store_true', help = 'prints the entire usage statement')
     args = parser.parse_args()
     
@@ -52,10 +54,9 @@ def find_athletes(athletes):
 
     connection.close()
 
-
 def find_gold_count():
     '''
-        Print the tally of gold medals one by representatives of each NOC
+        Print the tally of gold medals won by representatives of each NOC
     '''
     try:
         connection = psycopg2.connect(database=database, user=user, password=password)
@@ -84,9 +85,9 @@ def find_gold_count():
 
     connection.close()
 
-def find_avg_age():
+def find_avg_height():
     '''
-        Print the average age of the participants in every sport
+        Print the average height of the participants in every sport
     '''
     try:
         connection = psycopg2.connect(database=database, user=user, password=password)
@@ -108,29 +109,26 @@ def find_avg_age():
         print(e)
         exit()
 
-    print('===== Average Height of Participants for Every Sport ====')
+    print('===== Average Height of Participants for Every Sport (cm) ====')
     for row in cursor:
         print(row[0], "|", row[1])
     print()
 
     connection.close()
 
-#def print_usage():
- #   with open("usage.txt") as usage_file:
-  #      for line in usage_file:
-   #         print(line)
-            
-    #return
+def print_usage():
+    with open("usage.txt") as usage_file:
+        for line in usage_file:
+            print(line)
 
 def main():
     args = get_parsed_arguments()
-
     if args.athletes != None:
         find_athletes(args.athletes)
     if args.gold_count:
         find_gold_count()
-    if args.average_age:
-        find_avg_age()
+    if args.height_average:
+        find_avg_height() 
     if args.usage:
         print_usage()
 
