ff260023bac82d07c52775f5f1944202455c3fb7
rasmussenmatthew <rasmussenm@carelton.edu>
2021-02-01 10:21:32 -0600

    major changes, flushed out functions

--- olympics-api/olympics-api.py
+++ olympics-api/olympics-api.py
@@ -16,16 +16,16 @@ app = flask.Flask(__name__)
 
 @app.route('/games')
 def get_games():
- '''
-    a JSON list of dictionaries, each of which represents one
-    Olympic games, sorted by year. Each dictionary in this list will have
-    the following fields.
+    '''
+        a JSON list of dictionaries, each of which represents one
+        Olympic games, sorted by year. Each dictionary in this list will have
+        the following fields.
 
-    id -- (INTEGER) a unique identifier for the games in question
-    year -- (INTEGER) the 4-digit year in which the games were held (e.g. 1992)
-    season -- (TEXT) the season of the games (either "Summer" or "Winter")
-    city -- (TEXT) the host city (e.g. "Barcelona")
- '''
+        id -- (INTEGER) a unique identifier for the games in question
+        year -- (INTEGER) the 4-digit year in which the games were held (e.g. 1992)
+        season -- (TEXT) the season of the games (either "Summer" or "Winter")
+        city -- (TEXT) the host city (e.g. "Barcelona") 
+    '''
     try:
         connection = psycopg2.connect(database=database, user=user, password=password)
     except Exception as e:
@@ -41,54 +41,86 @@ def get_games():
         exit()
     
     results_list = []
-    games_dictionary = {}
+
     for row in cursor:
-        games_dictionary = {'id' = row[0], 'year' = row[1], 'season' = row[2], 'city' = row[3]}\
+        games_dictionary = {'id': row[0], 'year' : row[1], 'season' : row[2], 'city' : row[3]}
         results_list.append(games_dictionary)
     
     return json.dumps(results_list)
 
 @app.route('/noc')
 def get_noc():
- '''
-    a JSON list of dictionaries, each of which represents one
-    National Olympic Committee, alphabetized by NOC abbreviation. Each dictionary
-    in this list will have the following fields.
-
-
-    abbreviation -- (TEXT) the NOC's abbreviation (e.g. "USA", "MEX", "CAN", etc.)
-    name -- (TEXT) the NOC's full name (see the noc_regions.csv file)
- '''
     try:
         connection = psycopg2.connect(database=database, user=user, password=password)
     except Exception as e:
         print(e)
         exit()
+
+    try:
+        cursor = connection.cursor()
+        query = 'SELECT NOC, team FROM nations ORDER BY NOC'
+        cursor.execute(query)
+    except Exception as e:
+        print(e)
+        exit()
+
+    results_list = []
+    for row in cursor:
+        nations_dictionary = {'NOC' : row[0], 'region' : row[1]}
+        results_list.append(nations_dictionary)
+    
+    return json.dumps(results_list)
 
 
 @app.route('/medalists/games/<games_id>?[noc=noc_abbreviation]')
-def get_medalists(games_id, noc):
- '''
-    a JSON list of dictionaries, each representing one athlete
-    who earned a medal in the specified games. Each dictionary will have the
-    following fields.
-
-    athlete_id -- (INTEGER) a unique identifier for the athlete
-    athlete_name -- (INTEGER) a unique identifier for the athlete
-    athlete_sex -- (TEXT) the athlete's sex as specified in the database ("F" or "M")
-    sport -- (TEXT) the name of the sport in which the medal was earned
-    event -- (TEXT) the name of the event in which the medal was earned
-    medal -- (TEXT) the type of medal ("gold", "silver", or "bronze")
-
-    If the GET parameter noc=noc_abbreviation is present, this endpoint will return
-    only those medalists who were on the specified NOC's team during the specified
-    games.
- '''
+def get_medalists(games_id):
     try:
         connection = psycopg2.connect(database=database, user=user, password=password)
     except Exception as e:
         print(e)
         exit()
+    games_id = games_id
+    noc = flask.request.args.get('noc')
+    if noc is not None:
+        try:
+            cursor = connection.cursor()
+            query = '''SELECT DISTINCT athletes.id, athletes.athlete_name, athletes.sex, 
+                    contests.sport, contests.contest, contests_medals.medal 
+                    FROM athletes, contests_medals, athletes_games, contests, games
+                    WHERE athletes.id = athletes_games.athlete_id
+                    AND games.id = %s
+                    AND games.id = athletes_games.game_id
+                    AND athletes_games.id = contests_medals.athletes_games_id
+                    AND contests_medals.medal IS NOT NULL
+                    AND nations.id = athletes_games.nation_id
+                    AND contests.id = contests_medals.contest_id;'''
+            cursor.execute(query, (games_id,))
+        except Exception as e:
+            print(e)
+            exit()
+    else:
+        try:
+            cursor = connection.cursor()
+            query = '''SELECT DISTINCT athletes.id, athletes.athlete_name, athletes.sex, 
+                    contests.sport, contests.contest, contests_medals.medal 
+                    FROM athletes, contests_medals, athletes_games, contests, games
+                    WHERE athletes.id = athletes_games.athlete_id
+                    AND games.id = %s
+                    AND games.id = athletes_games.game_id
+                    AND athletes_games.id = contests_medals.athletes_games_id
+                    AND contests_medals.medal IS NOT NULL
+                    AND contests.id = contests_medals.contest_id;'''
+            cursor.execute(query, (games_id,))
+        except Exception as e:
+            print(e)
+            exit()
+
+    results_list = []
+    for row in cursor:
+        nations_dictionary = {'athleteid':row[0], 'athletename':row[1], 'athletesex':row[3], 'sport':row[4], 'event':[5], 'medal':row[6]}
+        results_list.append(nations_dictionary)
+
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('A sample Flask application/API')
