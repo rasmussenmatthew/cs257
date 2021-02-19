48ed8c70e9a9ad22c9168406f357f0474dcb629b
ignacio-nacho <rodriguezn@carleton.edu>
2021-02-18 22:45:57 -0600

    added api-tests.py, needs some work

---
+++ webapp-doc/tests/api-tests.py
@@ -0,0 +1,29 @@
+'''
+Nacho Rodriguez-Cortes
+Matthew Rasmussen
+2/19/2021
+'''
+#HOW DO WE CHECK CORRECT JSON RESPONSES!?!??!?!?!?!??!!??!?!??!
+
+
+#import api python code
+import unittest
+
+class WebAppChecker(unittest.TestCase):
+    def setUp(self):
+        url = "/"
+
+    def tearDown(self):
+        pass
+
+    def test_races(self):
+        #do we even?
+        pass
+
+    def test_races_example(self):
+        self.assertTrue(self.prime_checker.is_prime(2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
