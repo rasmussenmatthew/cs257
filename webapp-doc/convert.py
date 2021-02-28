ae2789de8f0f13e84fdfdf7a406726f876742fe3
rasmussenmatthew <rasmussenm@carleton.edu>
2021-02-28 16:01:16 -0600

    added to convert.py

--- webapp-doc/convert.py
+++ webapp-doc/convert.py
@@ -80,10 +80,87 @@ def make_spell_measurments_table():
     
     return spell_dict 
 
+def make_weapon_table():
+    weapon_dict = {}
 
+    with open('equipment.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        headers = next(csv_reader)
+        for row in csv_reader:
+            if "weapon" in row[4]:
+                weapon_name = row[3]
+                cost = row[6]
+                weight = row[7]
+                weapon_category = row[13]
+
+
+                split_string_dmg = row[16].split(',')
+                first_section = split_string_dmg[0]
+                last_section = split_string_dmg[2]
+                split_string_dmg = first_section.split(':')
+                damage_die = split_string_dmg[1]
+                #damage_die = damage_die[2:-1]
+                split_string_dmg = last_section.split(':')
+                damage_type = split_string_dmg[1]
+                #damage_type = long_range[:-1]
+
+                properties = row[18]
+                #ammuniation and loading are 'name :' first
+                
+
+
+                split_string_2h_dmg = row[19]
+                split_string_2h_dmg = split_string_2h_dmg.split(',')
+                first_section = split_string_2h_dmg[0]
+                split_string_2h_dmg = first_half.split(':')
+                two_handed_dmg = split_string_2h_dmg[1]
+                #two_handed_dmg = two_handed_dmg[2:-1]
+
+                if weapon_name not in weapon_dict:
+                    weapon_dict[weapon_name] = [len(weapon_dict) + 1, cost, weight, weapon_category, damage_die, damage_type, properties, two_handed_dmg]
+                
+    with open('weapons_new.csv', 'w', newline='') as new_csv_file:
+        writer = csv.writer(new_csv_file, delimiter=',')
+        for key in weapon_dict:
+            writer.writerow([weapon_dict[key][0], key, weapon_dict[key][1], weapon_dict[key][2], weapon_dict[key][3], weapon_dict[key][4], weapon_dict[key][5], weapon_dict[key][6], weapon_dict[key][7])  
+    return weapon_dict
+
+def make_weapon_range_table():
+    weapon_dict = {}
+    with open('equipment.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        headers = next(csv_reader)
+        spell_id = 1
+        for row in csv_reader:
+            if "weapon" in row[4]:
+                if row[17] != "":
+                    split_string = row[17].split(',')
+                    first_half = split_string[0]
+                    second_half = split_string[1]
+
+                    split_string = first_half.split(':')
+                    normal_range = split_string[1]
+                    #normal_range = normal_range[2:-1]
+
+                    split_string = second_half.split(':')
+                    long_range = split_string[1]
+                    long_range = long_range[:-1]
+
+                if weapon_id not in weapon_dict:
+                    weapon_dict[weapon_id] = [normal_range, long_range]
+                    spell_id += 1
+                                
+    with open('weapon_range.csv', 'w', newline='') as new_csv_file:
+        writer = csv.writer(new_csv_file, delimiter=',')
+        for key in weapon_dict:
+            writer.writerow([key, weapon_dict[key][0], weapon_dict[key][1])
+
+    return weapon_dict
 
 def main():
     spell_dict = make_spells_table()
     spell_measurments_dict = make_spell_measurments_table()
+    weapon_dict = make_weapon_table()
+    weapon_range_dict = make_weapon_range_table()
 
 main()
