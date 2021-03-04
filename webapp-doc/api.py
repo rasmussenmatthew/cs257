67c1d1274724bace401c3f1423a2fe19ec620c74
ignacio-nacho <rodriguezn@carleton.edu>
2021-03-04 09:40:22 -0600

    implemented api for spells within a specific class

--- webapp-doc/api.py
+++ webapp-doc/api.py
@@ -46,28 +46,24 @@ def get_spells():
 @api.route('/spells/classes/<class_name>')
 def get_spells_for_class(class_name):
     # Pay special attention to quotes! Originally not working when using single quotes on outside and double quotes for like clause
-    query = ''' 
-            SELECT spell_name, classes FROM spells WHERE classes LIKE '%Bard%' LIMIT 10; 
-            '''
+    like_arguments = '%' + class_name + '%'
+    query = '''SELECT spell_name, classes 
+               FROM spells 
+               WHERE classes LIKE %s ''' 
     spells_list = []
+
     try:
         connection = psycopg2.connect(database=database, user=user, password=password)
-    except Exception as e:
-        print(e)
-        exit()
-
-    try:
         cursor = connection.cursor()
-        cursor.execute(query)
+        cursor.execute(query, [like_arguments])
+        for row in cursor:
+            spells_dictionary = {'spell_name' : row[0], 'classes' : row[1]}
+            spells_list.append(spells_dictionary)
+        cursor.close()
+        connection.close()
     except Exception as e:
         print(e)
         exit()
-
-    for row in cursor:
-        spells_dictionary = {'spell_name' : row[0], 'classes' : row[1]}
-        spells_list.append(spells_dictionary)
-        
-    connection.close()
     '''
     spells_list = [{'spell_name':'acid arrow', 'spell_description':'fire an acid arrow', 'components':'[v,s]', 'ritual':'FALSE'},
                     {'spell_name':'fireball', 'spell_description':'giant exploding ball of fire', 'components':'[v,s]', 'ritual':'FALSE'}]
